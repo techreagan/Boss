b411768fb7022e81a8f815888d1dddf4e91bdd15
Reagan <reagan@Reagans-MacBook-Pro.local>
2018-10-16 09:33:53 +0100

    Base Controller Class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,12 +1,12 @@
 <?php 
 
-class Pages {
+class Pages extends Controller {
   public function __construct() {
     // echo 'Pages loaded';
   }
 
   public function index() {
-    
+    $this->view('hello');
   }
 
   public function about($id) {
