9d890500475c689484c6c12cc595f27daf2cafae
Reagan <reagan@Reagans-MacBook-Pro.local>
2018-10-16 10:15:02 +0100

    File Autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,14 @@
 <?php 
+// Load Config
+require_once 'config/config.php';
+
 // Load Libraries
-require_once 'libraries/core.php';
-require_once 'libraries/controller.php';
-require_once 'libraries/database.php';
+// require_once 'libraries/core.php';
+// require_once 'libraries/controller.php';
+// require_once 'libraries/database.php';
+
+
+// Autoload Core Libraries
+spl_autoload_register(function($className) {
+  require_once 'libraries/'. $className .'.php';
+});
