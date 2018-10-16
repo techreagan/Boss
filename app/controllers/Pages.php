2b7833c6fc81f2d73d1a5a78234cfbff63fee1f1
Reagan <reagan@Reagans-MacBook-Pro.local>
2018-10-16 09:21:26 +0100

    Mapping Methods And Parameters

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,6 +2,14 @@
 
 class Pages {
   public function __construct() {
-    echo 'Pages loaded';
+    // echo 'Pages loaded';
+  }
+
+  public function index() {
+    
+  }
+
+  public function about($id) {
+    echo 'This is about ' . $id;
   }
 }
--- app/libraries/core.php
+++ app/libraries/core.php
@@ -27,10 +27,23 @@ class Core {
 
     // Instantiate controller class
     $this->currentController = new $this->currentController;
-
     
-  }
+    // Check for second part of url
+    if(isset($url[1])) {
+      // Check to see if method exists in controller
+      if(method_exists($this->currentController, $url[1])) {
+        $this->currentMethod = $url[1];
+        // Unset 1 index
+        unset($url[1]);
+      }
+    }
 
+    // Get params
+    $this->params = $url ? array_values($url) : [];
+
+    // Call a callback with array of params 
+    call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+  }
 
   public function getUrl() {
     if(isset($_GET['url'])) {
