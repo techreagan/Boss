4659de0f89a794ce53a00e3c1ff0ef854198f30f
Reagan <reagan@Reagans-MacBook-Pro.local>
2018-10-18 08:54:41 +0100

    Clean Up

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,13 @@
 
 // DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'rmvc');
+define('DB_USER', '_YOUR_DB_USERNAME_');
+define('DB_PASS', '_YOUR_DB_PASSWORD_');
+define('DB_NAME', 'YOUR_DBNAME_');
 
 // App Root 
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost:8888/reaganmvc');
+define('URLROOT', '_YOUR_URL_');
 // Site Name
-define('SITENAME', 'ReaganMVC');
+define('SITENAME', '_YOUR_SITENAME_');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,17 +2,13 @@
 
 class Pages extends Controller {
   public function __construct() {
-    $this->postModel = $this->model('Post');
+
   }
 
   public function index() {
-    $posts = $this->postModel->getPosts();
     $data = [
-      'title' => 'Welcome',
-      'posts' => $posts
+      'title' => 'Boss'
     ];
-
-    
 
     $this->view('pages/index', $data);
   }
