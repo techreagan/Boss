c8efdb566a0f05e03cd28a9619a9fc1f389dd90a
Reagan <reagan@Reagans-MacBook-Pro.local>
2018-10-24 19:36:42 +0100

    Flash Session Message Function Added To Helper Functions

--- app/helpers/functions.php
+++ app/helpers/functions.php
@@ -41,6 +41,31 @@ function get_and_clear_message() {
   }
 }
 
+// Flash message helper 
+// EXAMPLE - flash('register_success', 'You are now registered', 'alert alert-danger');
+// DISPLAY IN VIEW - echo flash('register _success)
+function flash($name = '', $message = '', $class = 'alert alert-success') {
+  if(!empty($name)) {
+    if(!empty($message) && empty($_SESSION[$name])) {
+      if(!empty($_SESSION[$name])) {
+        unset($_SESSION[$name]);
+      }
+
+      if(!empty($_SESSION[$name . '_class'])) {
+        unset($_SESSION[$name . '_class']);
+      }
+     
+      $_SESSION[$name] = $message;
+      $_SESSION[$name . '_class'] = $class;
+    } elseif(empty($message) && !empty($_SESSION[$name])) {
+      $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
+      echo '<div class="'. $class .'" id="msg-flash">'. $_SESSION[$name] .'</div>';
+      unset($_SESSION[$name]);
+      unset($_SESSION[$name . '_class']);
+    }
+  }
+}
+
 function get_random_string() {
   return bin2hex(openssl_random_pseudo_bytes(32));
 }
